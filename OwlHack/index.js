8581030f21790a7052529263f30d04f07821bc57
Tu <tunhatha01@gmail.com>
2023-02-11 23:41:41 -0500

    two more maps added

--- OwlHack/index.js
+++ OwlHack/index.js
@@ -17,6 +17,24 @@ function toggleDiv(divid)
     }
 } 
 
+// function lonlat() {
+	
+// 	var input = document.getElementById('zip');	
+// 	var lonlat = document.getElementById('lonlat');
+	
+// 	if (event.keyCode === 13 && input.value.length === 5) {
+
+// 		var zipCode = input.value;
+
+// 		var xhr = $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + zipCode + '&key=AIzaSyDVPLLlJAQ679Frd0gu11khJ9mW02wsvWQ');
+
+// 		xhr.done(function(data) {
+// 			lonlat.innerHTML = 'Latitude = ' + data.results[0].geometry.location.lat + ' ' + '<br>Longitude = ' + data.results[0].geometry.location.lng;
+// 		});
+
+// 	} 
+//}
+
 
 
 
